a889337f2f3805a4240373556c62c7e315439981
Artem Startsev <artem.startsev@gmail.com>
2025-06-06 10:51:29 +0200

    Add description to the plugin

--- Infrastructure/Plugins/DatePlugin.cs
+++ Infrastructure/Plugins/DatePlugin.cs
@@ -1,10 +1,11 @@
+using System.ComponentModel;
 using Microsoft.SemanticKernel;
 
 namespace TeleBrief.Infrastructure.Plugins;
 
 public class DatePlugin
 {
-    [KernelFunction]
+    [KernelFunction, Description("Returns the current date in UTC in the format yyyy-MM-dd")]
     public string GetTodayDate()
     {
         return DateTime.UtcNow.ToString("yyyy-MM-dd");
